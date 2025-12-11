c8e905ecee714a7abf25b2342e0e74ef828bcf60
LongLQ25 <quanglong3145@gmail.com>
2025-12-11 13:54:06 +0700

    Update HangfireAuthorizationFilter.cs

--- MeetingSupportPlatform/MSP.WebAPI/Filters/HangfireAuthorizationFilter.cs
+++ MeetingSupportPlatform/MSP.WebAPI/Filters/HangfireAuthorizationFilter.cs
@@ -1,4 +1,7 @@
 ﻿using Hangfire.Dashboard;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http.Headers;
+using System.Text;
 
 namespace MSP.WebAPI.Filters
 {
@@ -9,13 +12,31 @@ namespace MSP.WebAPI.Filters
     /// </summary>
     public class HangfireAuthorizationFilter : IDashboardAuthorizationFilter
     {
+        private readonly string _username = "admin@gmail.com"; // Change this
+        private readonly string _password = "1"; // Change this
+
         public bool Authorize(DashboardContext context)
         {
             var httpContext = context.GetHttpContext();
-            
-            // Chỉ cho phép Admin role
-            return httpContext.User.Identity?.IsAuthenticated == true 
-                   && httpContext.User.IsInRole("Admin");
+            string authHeader = httpContext.Request.Headers["Authorization"];
+
+            if (authHeader != null && authHeader.StartsWith("Basic "))
+            {
+                var encodedUsernamePassword = authHeader.Split(' ', 2, StringSplitOptions.RemoveEmptyEntries)[1]?.Trim();
+                var decodedUsernamePassword = Encoding.UTF8.GetString(Convert.FromBase64String(encodedUsernamePassword));
+                var username = decodedUsernamePassword.Split(':', 2)[0];
+                var password = decodedUsernamePassword.Split(':', 2)[1];
+
+                if (username == _username && password == _password)
+                {
+                    return true;
+                }
+            }
+
+            // Challenge for Basic Auth
+            httpContext.Response.StatusCode = 401;
+            httpContext.Response.Headers["WWW-Authenticate"] = "Basic realm=\"Hangfire Dashboard\"";
+            return false;
         }
     }
 }
